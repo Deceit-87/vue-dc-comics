e38dad7a4887b7171bd3bae1c1c102bcf667a6fc
Deceit-87 <manuelfreund318@gmail.com>
2022-03-10 19:48:37 +0100

    aggiunti sezione jubotron , vfor components

---
+++ src/components/CardComics.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card-wrapper">
+      <img :src="img" >
+      <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['img','title']
+}
+</script>
+
+<style scoped lang="scss">
+
+.card-wrapper{
+    width: 15%;
+    img{
+        width: 100%;
+        object-position: top;
+        object-fit: cover;
+        aspect-ratio: 1/1;
+    }
+    h2{
+        font-size: 12px;
+    }
+}
+</style>
